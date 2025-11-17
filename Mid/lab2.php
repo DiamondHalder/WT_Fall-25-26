406923f8bb68a48b0a0e1eaf42e88d9a8f2e3963
ayeshaaa02 <22-49442-3@student.aiub.edu>
2025-11-17 11:12:19 +0600

    made form

--- Mid/lab2.php
+++ Mid/lab2.php
@@ -5,18 +5,24 @@
 <body>
     <center><h1>Student Registration</h1></center>
     <br>
-    Full Name: <br>
-    <input type="text" required> <br> <br>
-    Email: <br>
-    <input type="text" required> <br> <br>
-    Password: <br>
-    <input type="password" required> <br> <br>
-    Confirm Password: <br> 
-    <input type="password" required> <br> <br>
-    <button>Register </button>
-    Registration Successful!
-    Name: 
-    Email: 
+    <form onsubmit="return validateForm()">
+        Full Name: <br>
+        <input type="text" id="fullName" required> <br> <br>
+        Email: <br>
+        <input type="text" id="email" required> <br> <br>
+        Password: <br>
+        <input type="password" id="password" required> <br> <br>
+        Confirm Password: <br> 
+        <input type="password" id="confirmPassword" required> <br> <br>
+        <button type="submit">Register </button>
+    </form>
+    
+    <div id="successMsg" style="display:none;">
+        <br><br>
+        Registration Successful!<br>
+        Name: <span id="displayName"></span><br>
+        Email: <span id="displayEmail"></span>
+    </div>
     <center><h1>Course Registration</h1></center>
     <button>Add Course</button>
     
