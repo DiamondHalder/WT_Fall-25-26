ff805830117f88c02a562e42a0e9dfd4da8ce6a3
ayeshaaa02 <22-49442-3@student.aiub.edu>
2025-11-17 11:05:54 +0600

    initial structure

---
+++ Mid/lab2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+ <title>Student Registration</title>
+</head>
+<body>
+    <center><h1>Student Registration</h1></center>
+    <br>
+    Full Name: <br>
+    <input type="text" required> <br> <br>
+    Email: <br>
+    <input type="text" required> <br> <br>
+    Password: <br>
+    <input type="password" required> <br> <br>
+    Confirm Password: <br> 
+    <input type="password" required> <br> <br>
+    <button>Register </button>
+    Registration Successful!
+    Name: 
+    Email: 
+    <center><h1>Course Registration</h1></center>
+    <button>Add Course</button>
+    
+</body>
+</html>
