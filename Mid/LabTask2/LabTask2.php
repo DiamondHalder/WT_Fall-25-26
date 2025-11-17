a242e55b59ff70f986d09e1d7451be078b5d5060
DiamondHalder <diamond.halder28@gmail.com>
2025-11-17 11:13:48 +0600

    2 commit

---
+++ Mid/LabTask2/LabTask2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Registrarion</title>
+    </head>
+     <body>
+        <h1 align="center">Participant Registration</h1>
+        Full Name:
+        <input type="text" id="name">
+        
+
+     </body>
+    <style>
+        body {
+            font-family: 'Times New Roman', Times, serif;
+            padding: 30px;
+            background-color:aqua ;
+        }
+        
+    </style>
+    
+</html>
