da34f0a8041ad1c16172569b96df5be4879a5d62
DiamondHalder <diamond.halder28@gmail.com>
2025-11-29 15:04:57 +0600

    Portfolio

---
+++ Mid/Html_Practice_Task/Lt-1.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<head>
+    <title>Portfolio</title>
+</head>
+
+<body>
+    <header>
+        <h1>Diamond Halder</h1>
+        <nav>
+            <ul>
+                <li><a href="#bio">Biography</a></li>
+                <li><a href="#education">Education</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section id="bio">
+            <h2>About Me</h2>
+            <p>Biography</p>
+            
+        </section>
+        <section id="education">
+            <h2>Education</h2>
+            <table>
+                <tr>
+                    <th>Degree</th>
+                    <th>Institution</th>
+                    <th>Year</th>
+                    <th>Result</th>
+                </tr>
+
+                <tr>
+                    <td>Bachelor of Computer Science</td>
+                    <td>American International University Bangladesh</td>
+                    <td>2026</td>
+                </tr>
+
+                <tr>
+                    <td>HSC</td>
+                    <td>BM College</td>
+                    <td>2022</td>
+                </tr>
+
+                <tr>
+                    <td>SSC</td>
+                    <td>K. M. Latif Institution </td>
+                    <td>2019</td>
+                </tr>
+
+
+            </table>
+
+        </section>
+    </main>
+</body>
