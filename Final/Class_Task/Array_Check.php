9b84ff1385699241f00c46d67eec2ef115a22fbd
DiamondHalder <diamond.halder28@gmail.com>
2025-12-08 12:19:04 +0600

    Class Task

---
+++ Final/Class_Task/Array_Check.php
@@ -0,0 +1,23 @@
+<?php
+$number = array(80, 40, 60);
+
+$highest= $number[0];
+
+for ($i = 1; $i < 5; $i++) {
+   
+    if ($number[$i] > $highest) {
+        $highest = $number[$i];
+    }
+}
+
+echo "Highest Number: " . $highest . "<br>";
+
+
+
+if ($highest % 2 == 0) {
+    echo "The highest number is Even<br>"; 
+} else {
+    echo "The highest number is Odd<br>"; 
+}
+
+?>
