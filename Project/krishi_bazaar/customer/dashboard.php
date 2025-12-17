71b8c096de7c25253a2e050ce03ad31f57e2cb07
DiamondHalder <diamond.halder28@gmail.com>
2025-12-17 21:29:12 +0600

    update

---
+++ Project/krishi_bazaar/customer/dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <?php include("../includes/head.php"); ?>
+       
+    </head>
+    <body>
+        <?php include("../includes/header.php"); ?>
+
+        <div class="container">
+
+        <div class="sidebar">
+            <a href="dashboard.php">Dashboard</a>
+            <a href="browse_products.php">Browse Products</a>
+            <a href="my_orders.php">My Orders</a>
+            <a href="cart.php">Cart</a>
+            <a href="profile.php">Profile</a>
+        </div>
+
+        <div class="content">
+            <h2>Customer Dashboard</h2><br>
+
+
+        </div>
+       
+    </body>
+</html>
