7cfef4a7902c1db03be4c8ba56c1ebcd8a23b67a
DiamondHalder <diamond.halder28@gmail.com>
2025-12-19 00:10:04 +0600

    update

--- Project/krishi_bazaar/customer/profile.php
+++ Project/krishi_bazaar/customer/profile.php
@@ -34,10 +34,35 @@
 
             if($name==""||$email==""||$age==""||$gender==""||$phone==""||$address==""){
                 echo "<p style='color:red;'>All fields are required.</p>";
+            }elseif(!is_numeric($age) || $age< 1){
+                echo "<p style='color:red;'>Please Enter a Valid Age.</p>";
+            }else{
+                echo "<p style='color:green;'>Profile updated successfull</p>";
             }
 
            }
             ?>
+
+            <form method="post" class="profile-form">
+                <label>Name</label>
+                <input type="text" name="name" value="Rabbi">
+                <label>Email</label>
+                <input type="email" name="email" value="a@gmail.com">
+                <label>Age</label>
+                <input type="number" name="age" value="22">
+                <label>Gender</label>
+                <select name="gender">
+                    <option value="">Select</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+                <label>Phone</label>
+                <input type="text" name="Phone" value="0171111111111">
+                <label>Address</label>
+                <textarea name="address">Dhaka</textarea>
+                <br>
+                <button type="submit" value="Update Profie">Update Profile</button>
+            </form>
 
 
         </div>
