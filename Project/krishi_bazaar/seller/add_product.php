7c9c761c445928cf26b9fe2993fc0016ca93db08
DiamondHalder <diamond.halder28@gmail.com>
2025-12-20 21:01:55 +0600

    update

---
+++ Project/krishi_bazaar/seller/add_product.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <?php include("../includes/head.php"); ?>
+
+</head>
+
+<body>
+    <?php include("../includes/header.php"); ?>
+
+    <div class="container">
+
+        <div class="sidebar">
+            <a href="dashboard.php">Dashboard</a>
+            <a href="add_product.php"></a>
+            <a href="my_products.php">My Products</a>
+            <a href="orders.php">Orders</a>
+            <a href="profile.php">Profile</a>
+        </div>
+
+        <div class="content">
+            <h2>Add Product</h2><br>
+            <div class="card-container">
+                <div class="card">
+                    <h3>Total Orders</h3>
+                    <p>---</p>
+
+                </div>
+                <div class="card">
+                    <h3>Pending Orders</h3>
+                    <p>---</p>
+
+                </div>
+                <div class="card">
+                    <h3>Total Sales</h3>
+                    <p>---</p>
+
+                </div>
+                
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <?php include("../includes/footer.php"); ?>
+
+</body>
+
+</html>
