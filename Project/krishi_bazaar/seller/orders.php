1163200ecd52491d47f6c88b4fcefa92e141ee3d
DiamondHalder <diamond.halder28@gmail.com>
2025-12-22 21:25:15 +0600

    update

--- Project/krishi_bazaar/seller/orders.php
+++ Project/krishi_bazaar/seller/orders.php
@@ -5,6 +5,20 @@ $orders= [
 ['order_id'=>503,'customer'=>'Karim','product'=>'Onion','quantity'=>3,'total'=>180,'status'=>'shipped'],
 ['order_id'=>504,'customer'=>'Jamal','product'=>'Rice','quantity'=>2,'total'=>100,'status'=>'none']
 ];
+
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $order_id=$_POST['order_id'];
+    foreach($orders as &$order){
+        if($order['order_id']==$order_id){
+            if(isset($_POST['confirm'])){
+                $order['status']='shipped';
+                $message="Order Id $order_id confirmed and sipped.";
+
+            }elseif (isset($_POST))
+        }
+    }
+}
 ?>
 
 
