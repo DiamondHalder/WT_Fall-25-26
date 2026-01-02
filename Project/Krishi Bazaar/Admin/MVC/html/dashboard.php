217e7496e3e31b5027c77b260fa08d5f6a289897
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-02 18:24:29 +0600

    added dashboard styling to the page

--- Project/Krishi Bazaar/Admin/MVC/html/dashboard.php
+++ Project/Krishi Bazaar/Admin/MVC/html/dashboard.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="dashboard.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
 </head>
 <body class="dashboard-page">
     <div class="header">
