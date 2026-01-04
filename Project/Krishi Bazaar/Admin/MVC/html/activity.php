e3b1e2d4a2c04f73674b6161b85fc7c7ba5d30ba
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-04 20:51:41 +0600

    add activity page

---
+++ Project/Krishi Bazaar/Admin/MVC/html/activity.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Activity Monitor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="dashboard-page">
+    <div class="header">
+        <div class="user-info">
+            <span>Welcome, Admin</span>
+            <a href="login.php" class="logout-btn">Logout</a>
+        </div>
+    </div>
+</body>
+</html>
