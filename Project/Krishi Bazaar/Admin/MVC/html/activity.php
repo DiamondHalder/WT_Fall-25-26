7172012f2cd101ca6fab147f3f497d4a8a1f4fba
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-04 20:57:30 +0600

    added sidebar menu to activity monitor page

--- Project/Krishi Bazaar/Admin/MVC/html/activity.php
+++ Project/Krishi Bazaar/Admin/MVC/html/activity.php
@@ -11,5 +11,23 @@
             <a href="login.php" class="logout-btn">Logout</a>
         </div>
     </div>
+    <div class="container">
+        <div class="sidebar">
+            <h3>Menu</h3>
+            <ul>
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="statistics.php">Statistics</a></li>
+                <li><a href="orders.php">Manage Orders</a></li>
+                <li><a href="users.php">Manage Users & Sellers</a></li>
+                <li><a href="products.php">Product Moderation</a></li>
+                <li class="active"><a href="activity.php">Activity Monitor</a></li>
+            </ul>
+        </div>
+        <div class="main-content">
+            
+        </div>
+
+
+    </div>
 </body>
 </html>
