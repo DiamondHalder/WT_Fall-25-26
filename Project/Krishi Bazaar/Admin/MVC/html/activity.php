76231bfe33e7b880a197eaf982c5b5dd9bec29ab
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-04 21:50:18 +0600

    updated activity monitor page

--- Project/Krishi Bazaar/Admin/MVC/html/activity.php
+++ Project/Krishi Bazaar/Admin/MVC/html/activity.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <title>Activity Monitor</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/statistics.css">
 </head>
 <body class="dashboard-page">
     <div class="header">
@@ -24,10 +25,48 @@
             </ul>
         </div>
         <div class="main-content">
-            
+            <h2>Activity Monitor</h2>
+            <div class="simple-table">
+                <table>
+                    <tr>
+                        <th>Time</th>
+                        <th>User</th>
+                        <th>Activity</th>
+                        <th>Details</th>
+                    </tr>
+                    <tr>
+                        <td>10:30 AM</td>
+                        <td>Admin</td>
+                        <td>Logged In</td>
+                        <td>From IP: 192.168.1.1</td>
+                    </tr>
+                    <tr>
+                        <td>10:25 AM</td>
+                        <td>User: U1001</td>
+                        <td>Placed Order</td>
+                        <td>Order #10025</td>
+                    </tr>
+                    <tr>
+                        <td>10:15 AM</td>
+                        <td>Seller: S2001</td>
+                        <td>Added Product</td>
+                        <td>Product: Fresh Rice</td>
+                    </tr>
+                    <tr>
+                        <td>09:45 AM</td>
+                        <td>System</td>
+                        <td>User Registered</td>
+                        <td>New user: Faiyad Hoque</td>
+                    </tr>
+                    <tr>
+                        <td>Yesterday</td>
+                        <td>Admin</td>
+                        <td>Blocked User</td>
+                        <td>User ID: U887 blocked</td>
+                    </tr>
+                </table>
+            </div>
         </div>
-
-
     </div>
 </body>
 </html>
