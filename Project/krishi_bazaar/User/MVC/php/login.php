2ddf1f2a1f19a0e328880ce344b5510a4dddc859
DiamondHalder <diamond.halder28@gmail.com>
2026-01-14 15:42:27 +0600

    update

--- Project/krishi_bazaar/User/MVC/php/login.php
+++ Project/krishi_bazaar/User/MVC/php/login.php
@@ -1,52 +1,30 @@
 <?php
 session_start();
+include("../db/db.php");
 
 $error = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $role = $_POST['role'] ?? '';
-    $email = trim($_POST['email'] ?? '');
-    $password = trim($_POST['password'] ?? '');
+    
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
 
-    if ($role == "" || $email == "" || $password == "") {
-        $error = "All fields are required.";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $error = "Invalid email format.";
+    if ($email == "" || $password == "") {
+        $error = "Email and Password required.";
+     
     } else {
-        if ($role === "admin") {
-            if ($email === "admin@krishibazar.com" && $password === "1234") {
-                $_SESSION['user_role'] = 'admin';
-                $_SESSION['admin_email'] = $email;
-                header("Location: ../html/admin_dashboard.php");
-                exit;
-            } else {
-                $error = "Invalid admin credentials.";
-            }
+        $sql = "SELECT * FROM sellers WHERE email ='$email'";
+        $result = $conn->query($sql);
+
+        if($result->num_rows ==1){
+            $seller = $result->fetch_assoc();
+
+            if 
         }
 
-        if ($role === "seller") {
-            $_SESSION['user_role'] = 'seller';
-            $_SESSION['seller_id'] = 1;
-            $_SESSION['seller_email'] = $email;
+        
 
-            setcookie("user_role", "seller", time() + 3600, "/");
-            setcookie("user_email", $email, time() + 3600, "/");
-
-            header("Location: ../html/seller_dashboard.php");
-            exit;
-        }
-
-        if ($role === "customer") {
-            $_SESSION['user_role'] = 'customer';
-            $_SESSION['customer_id'] = 1;
-            $_SESSION['customer_email'] = $email;
-
-            setcookie("user_role", "customer", time() + 3600, "/");
-            setcookie("user_email", $email, time() + 3600, "/");
-
-            header("Location: ../html/customer_dashboard.php");
-            exit;
-        }
+        
     }
 }
 
