570f3000f2eb8b6fd1391673a5cecb48c5aecdff
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-16 00:22:55 +0600

    added session variables and update links

--- Project/Krishi Bazaar/Admin/MVC/html/activity.php
+++ Project/Krishi Bazaar/Admin/MVC/html/activity.php
@@ -1,3 +1,9 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+$admin_name = isset($_SESSION["admin_name"]) ? $_SESSION["admin_name"] : "Admin";
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,22 +14,22 @@
 <body class="dashboard-page">
     <div class="header">
         <div class="user-info">
-            <span>Welcome, Admin</span>
-            <a href="login.php" class="logout-btn">Logout</a>
+            <span>Welcome, <?php echo $admin_name; ?></span>
+            <a href="../php/logout_controller.php" class="logout-btn">Logout</a>
         </div>
     </div>
     <div class="container">
         <div class="sidebar">
-            <h3>Menu</h3>
-            <ul>
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="statistics.php">Statistics</a></li>
-                <li><a href="orders.php">Manage Orders</a></li>
-                <li><a href="users.php">Manage Users & Sellers</a></li>
-                <li><a href="products.php">Product Moderation</a></li>
-                <li class="active"><a href="activity.php">Activity Monitor</a></li>
-            </ul>
-        </div>
+    <h3>Menu</h3>
+    <ul>
+        <li><a href="../php/dashboard_controller.php">Dashboard</a></li>
+        <li><a href="../php/statistics_controller.php">Statistics</a></li>
+        <li><a href="../php/orders_controller.php">Manage Orders</a></li>
+        <li><a href="../php/users_controller.php">Manage Users & Sellers</a></li>
+        <li><a href="../php/products_controller.php">Product Moderation</a></li>
+        <li class="active"><a href="../php/activity_controller.php">Activity Monitor</a></li>
+    </ul>
+</div>
         <div class="main-content">
             <h2>Activity Monitor</h2>
             <div class="simple-table">
