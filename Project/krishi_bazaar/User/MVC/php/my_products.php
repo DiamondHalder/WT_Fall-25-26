9db18bdf858007b9ac3795e9d25177b6dc41a6f3
DiamondHalder <diamond.halder28@gmail.com>
2026-01-15 14:04:23 +0600

    update

--- Project/krishi_bazaar/User/MVC/php/my_products.php
+++ Project/krishi_bazaar/User/MVC/php/my_products.php
@@ -2,24 +2,34 @@
 require_once("seller_auth.php"); 
 include("../db/db.php");
 
+$seller_id = $_SESSION["seller_id"];
+
 $message = "";
 $error = "";
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
 
     if(isset($_POST['delete_product'])){
-        $product_id = $_POST['product_id'];
-        
-        $error = "Product Id $product_id deleted.";
+        $product_id = intval($_POST['product_id']);
+
+        $sql = "DELETE FROM products WHERE product_id='$product_id' AND seller_id='$seller_id'";
+        if(!$conn->query($sql)){
+            $error = "Delete Failed " . $conn->error;
+        } else {
+
+        $message = "Product Id deleted successfully.";
+    }
     }
 
     if(isset($_POST['edit_product'])){
-        $product_id = $_POST['product_id'];
-        // Redirect to edit page with product_id
+        $product_id = intval($_POST['product_id']);
+        
         header("Location: edit_products.php?product_id=$product_id");
-        exit;
+        exit();
     }
 }
+
+$products = [];
 
 
 include("../html/my_products.php");
