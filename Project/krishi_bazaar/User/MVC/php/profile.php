94d8932df9412bf5541cfacea9ef872791269928
DiamondHalder <diamond.halder28@gmail.com>
2026-01-16 02:57:26 +0600

    update

--- Project/krishi_bazaar/User/MVC/php/profile.php
+++ Project/krishi_bazaar/User/MVC/php/profile.php
@@ -1,27 +1,30 @@
 <?php
-require_once("../php/seller_auth.php");  
+require_once("../php/seller_auth.php");
+include("../db/db.php");  
 
 
-$seller = $_SESSION['seller'] ?? [
-    'name' => 'Sagor',
-    'email' => 'Sagor@gmail.com',
-    'phone' => '123452341234',
-    'shop_name' => 'Sagor shop',
-    'address' => 'Barishal',
-    'password' => '1234'
-];
+$seller = $_SESSION['seller'] ;
 
 
 $message = "";
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['update_profile'])) {
-    $seller['name'] = $_POST['name'];
-    $seller['email'] = $_POST['email'];
-    $seller['phone'] = $_POST['phone'];
-    $seller['shop_name'] = $_POST['shop_name'];
-    $seller['address'] = $_POST['address'];
-    $seller['password'] = $_POST['password'];
+$error= "";
+$sql=$conn->query("SELECT * FROM sellers WHERE seller_id = '$seller_id'");
+$seller=$sql->fetch_assoc();
 
-    $message = "Profile updated successfully";
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['update_profile'])) {
+    $name= $_POST['name'];
+    $email= $_POST['email'];
+    $phone= $_POST['phone'];
+    $shop_name = $_POST['shop_name'];
+    $address= $_POST['address'];
+    $password = $_POST['password'];
+
+    $update_sql = "UPDATE sellers SET name='$name', email='$email', phone='$phone', shop_name='$shop_name', address='$address', password='$password' WHERE seller_id='$seller_id'";
+    if($conn->query($update_sql)){
+        $message = "Profile updated successfully";
+        
+    }
+    
 
     
     $_SESSION['seller'] = $seller;
