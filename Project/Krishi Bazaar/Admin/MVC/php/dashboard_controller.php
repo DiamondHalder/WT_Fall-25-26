fbc16c7f7e951e8e44c70e16df2928fab65a1776
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-16 00:26:36 +0600

    added dashboard controller

---
+++ Project/Krishi Bazaar/Admin/MVC/php/dashboard_controller.php
@@ -0,0 +1,38 @@
+<?php
+require_once "session_check.php";
+include "../db/config.php";
+
+$admin_name = $_SESSION["admin_name"] ?? "Admin";
+
+$total_users = 0;
+$total_sellers = 0;
+$total_products = 0;
+$total_orders = 0;
+
+$sql1 = "SELECT COUNT(*) as count FROM users WHERE type='customer'";
+$result1 = mysqli_query($conn, $sql1);
+$row1 = mysqli_fetch_assoc($result1);
+$total_users = $row1['count'];
+
+$sql2 = "SELECT COUNT(*) as count FROM users WHERE type='seller'";
+$result2 = mysqli_query($conn, $sql2);
+$row2 = mysqli_fetch_assoc($result2);
+$total_sellers = $row2['count'];
+
+$sql3 = "SELECT COUNT(*) as count FROM products";
+$result3 = mysqli_query($conn, $sql3);
+$row3 = mysqli_fetch_assoc($result3);
+$total_products = $row3['count'];
+
+$data = [
+    'admin_name' => $admin_name,
+    'total_users' => $total_users,
+    'total_sellers' => $total_sellers,
+    'total_products' => $total_products,
+    'total_orders' => $total_orders
+];
+
+
+require_once "../html/dashboard.php";
+mysqli_close($conn);
+?>
