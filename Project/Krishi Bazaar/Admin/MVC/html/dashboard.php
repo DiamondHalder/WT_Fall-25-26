40e31a88f09109c62cb731315a60d6b761ca4d0f
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-16 00:24:24 +0600

    added data validation

--- Project/Krishi Bazaar/Admin/MVC/html/dashboard.php
+++ Project/Krishi Bazaar/Admin/MVC/html/dashboard.php
@@ -1,3 +1,10 @@
+<?php
+if (!isset($data)) 
+{
+    header("Location: ../php/login_controller.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +14,8 @@
 <body class="dashboard-page">
     <div class="header">
         <div class="user-info">
-            <span>Welcome, Admin</span>
-            <a href="login.php" class="logout-btn">Logout</a>
+            <span>Welcome, <?php echo $data['admin_name']; ?></span>
+            <a href="../php/logout_controller.php" class="logout-btn">Logout</a>
         </div>
     </div>
  
@@ -16,36 +23,36 @@
         <div class="sidebar">
             <h3>Menu</h3>
             <ul>
-                <li class="active"><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="statistics.php">Statistics</a></li>
-                <li><a href="orders.php">Manage Orders</a></li>
-                <li><a href="users.php">Manage Users & Sellers</a></li>
-                <li><a href="products.php">Product Moderation</a></li>
-                <li><a href="activity.php">Activity Monitor</a></li>
+                <li class="active"><a href="../php/dashboard_controller.php">Dashboard</a></li>
+                <li><a href="../php/statistics_controller.php">Statistics</a></li>
+                <li><a href="../php/orders_controller.php">Manage Orders</a></li>
+                <li><a href="../php/users_controller.php">Manage Users & Sellers</a></li>
+                <li><a href="../php/products_controller.php">Product Moderation</a></li>
+                <li><a href="../php/activity_controller.php">Activity Monitor</a></li>
             </ul>
         </div>
  
         <div class="main-content">
-       
             <div class="cards-container">
+                
                 <div class="card">
                     <h3>Total Users</h3>
-                    <p class="number">1,245</p>
+                    <p class="number"><?php echo $data['total_users']; ?></p>
                 </div>
                
                 <div class="card">
                     <h3>Total Sellers</h3>
-                    <p class="number">356</p>
+                    <p class="number"><?php echo $data['total_sellers']; ?></p>
                 </div>
                
                 <div class="card">
                     <h3>Total Products</h3>
-                    <p class="number">5,820</p>
+                    <p class="number"><?php echo $data['total_products']; ?></p>
                 </div>
                
                 <div class="card">
                     <h3>Total Orders</h3>
-                    <p class="number">8,940</p>
+                    <p class="number"><?php echo $data['total_orders']; ?></p>
                 </div>
             </div>
         </div>
