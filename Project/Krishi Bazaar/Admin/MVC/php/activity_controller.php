589e60f29b798030c8cf3f5d9ade19b9aef0e13a
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-16 22:16:55 +0600

    fetch and display activity log from database

--- Project/Krishi Bazaar/Admin/MVC/php/activity_controller.php
+++ Project/Krishi Bazaar/Admin/MVC/php/activity_controller.php
@@ -1,4 +1,21 @@
 <?php
 require_once "session_check.php";
+include "../db/config.php";
+
+$sql = "SELECT * FROM activity_log ORDER BY activity_time DESC";
+$result = mysqli_query($conn, $sql);
+
+$activities = [];
+while ($row = mysqli_fetch_assoc($result)) 
+{
+    $activities[] = $row;
+}
+
+mysqli_close($conn);
+
+$data = [
+    'activities' => $activities
+];
+
 require_once "../html/activity.php";
 ?>
