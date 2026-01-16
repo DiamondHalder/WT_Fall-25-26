8543cb276336ccb6f95c070777be4debcbafa4ce
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-16 22:14:19 +0600

    improved session validation

--- Project/Krishi Bazaar/Admin/MVC/html/activity.php
+++ Project/Krishi Bazaar/Admin/MVC/html/activity.php
@@ -3,7 +3,16 @@ if (!isset($_SESSION)) {
     session_start();
 }
 $admin_name = isset($_SESSION["admin_name"]) ? $_SESSION["admin_name"] : "Admin";
+
+if (!isset($data)) 
+{
+    header("Location: ../php/login_controller.php");
+    exit();
+}
+
+$activities = $data['activities'];
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,56 +29,38 @@ $admin_name = isset($_SESSION["admin_name"]) ? $_SESSION["admin_name"] : "Admin"
     </div>
     <div class="container">
         <div class="sidebar">
-    <h3>Menu</h3>
-    <ul>
-        <li><a href="../php/dashboard_controller.php">Dashboard</a></li>
-        <li><a href="../php/statistics_controller.php">Statistics</a></li>
-        <li><a href="../php/orders_controller.php">Manage Orders</a></li>
-        <li><a href="../php/users_controller.php">Manage Users & Sellers</a></li>
-        <li><a href="../php/products_controller.php">Product Moderation</a></li>
-        <li class="active"><a href="../php/activity_controller.php">Activity Monitor</a></li>
-    </ul>
-</div>
+            <h3>Menu</h3>
+            <ul>
+                <li><a href="../php/dashboard_controller.php">Dashboard</a></li>
+                <li><a href="../php/statistics_controller.php">Statistics</a></li>
+                <li><a href="../php/orders_controller.php">Manage Orders</a></li>
+                <li><a href="../php/users_controller.php">Manage Users & Sellers</a></li>
+                <li><a href="../php/products_controller.php">Product Moderation</a></li>
+                <li class="active"><a href="../php/activity_controller.php">Activity Monitor</a></li>
+            </ul>
+        </div>
         <div class="main-content">
             <h2>Activity Monitor</h2>
             <div class="simple-table">
                 <table>
-                    <tr>
-                        <th>Time</th>
-                        <th>User</th>
-                        <th>Activity</th>
-                        <th>Details</th>
-                    </tr>
-                    <tr>
-                        <td>10:30 AM</td>
-                        <td>Admin</td>
-                        <td>Logged In</td>
-                        <td>From IP: 192.168.1.1</td>
-                    </tr>
-                    <tr>
-                        <td>10:25 AM</td>
-                        <td>User: U1001</td>
-                        <td>Placed Order</td>
-                        <td>Order #10025</td>
-                    </tr>
-                    <tr>
-                        <td>10:15 AM</td>
-                        <td>Seller: S2001</td>
-                        <td>Added Product</td>
-                        <td>Product: Fresh Rice</td>
-                    </tr>
-                    <tr>
-                        <td>09:45 AM</td>
-                        <td>System</td>
-                        <td>User Registered</td>
-                        <td>New user: Faiyad Hoque</td>
-                    </tr>
-                    <tr>
-                        <td>Yesterday</td>
-                        <td>Admin</td>
-                        <td>Blocked User</td>
-                        <td>User ID: U887 blocked</td>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>Time</th>
+                            <th>User</th>
+                            <th>Activity</th>
+                            <th>Details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($activities as $activity): ?>
+                            <tr>
+                                <td><?php echo date('h:i A', strtotime($activity['activity_time'])); ?></td>
+                                <td><?php echo $activity['user_type']; ?>: <?php echo $activity['user_name']; ?></td>
+                                <td><?php echo $activity['activity']; ?></td>
+                                <td><?php echo $activity['details']; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
                 </table>
             </div>
         </div>
