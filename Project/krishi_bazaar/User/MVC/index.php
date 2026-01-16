8eb1380279516ed8e09faec604848162092ab366
DiamondHalder <diamond.halder28@gmail.com>
2026-01-16 19:50:15 +0600

    update

---
+++ Project/krishi_bazaar/User/MVC/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: php/pindex.php");
+exit();
+?>
---
+++ Project/krishi_bazaar/User/MVC/php/pindex.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+if (isset($_SESSION['seller_id'])) {
+    header("Location: seller_dashboard.php");
+    exit();
+} else {
+    header("Location: login.php");
+    exit();
+}
+
+
+?>
