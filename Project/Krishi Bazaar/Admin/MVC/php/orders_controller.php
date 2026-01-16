9de30f2dcaaf5758f00728d845d610782b0eea10
ayeshaaa02 <22-49442-3@student.aiub.edu>
2026-01-16 22:20:00 +0600

    fetch and display orders from database

--- Project/Krishi Bazaar/Admin/MVC/php/orders_controller.php
+++ Project/Krishi Bazaar/Admin/MVC/php/orders_controller.php
@@ -1,4 +1,21 @@
 <?php
 require_once "session_check.php";
+include "../db/config.php";
+
+
+$sql = "SELECT * FROM orders ORDER BY order_date DESC";
+$result = mysqli_query($conn, $sql);
+
+$orders = [];
+while ($row = mysqli_fetch_assoc($result)) 
+{
+    $orders[] = $row;
+}
+
+mysqli_close($conn);
+
+$data = [
+    'orders' => $orders
+];
 require_once "../html/orders.php";
 ?>
