db50872d295dd715a62241984b6b1b28030654f9
DiamondHalder <diamond.halder28@gmail.com>
2026-01-16 23:34:50 +0600

    Removed unnecessary folders and kept only the project folder

--- Final/Class_Task_3/session/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy(); 
-
-echo "Logged out successfully!";
-echo "<br><a href='dashboard.php'>Back to Dashboard</a>";
-?>
--- Final/Labtask/SessionCookie/UserLoginSystemWithSession/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy(); // সেশন মুছে ফেলা 
-header("Location: login.php"); // লগইন পেজে ফেরত পাঠানো 
-exit();
-?>
