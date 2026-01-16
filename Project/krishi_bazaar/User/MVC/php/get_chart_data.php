22ff5331d4bd71c26944757cc4d3de41521b2504
DiamondHalder <diamond.halder28@gmail.com>
2026-01-16 10:56:18 +0600

    update

--- Project/krishi_bazaar/User/MVC/php/get_chart_data.php
+++ Project/krishi_bazaar/User/MVC/php/get_chart_data.php
@@ -3,3 +3,21 @@ require_once("seller_auth.php");
 include("../db/db.php");
 
 $seller_id = $_SESSION['seller_id'];
+//pie
+$pieQuery = $conn->query("SELECT p.name, SUM(o.total_price) AS total_value
+                          FROM orders o
+                          JOIN products p ON o.product_id = p.product_id
+                          WHERE o.seller_id='$seller_id'
+                          GROUP BY p.product_id");
+                          
+$pieRes=$conn->query($pieQuery);
+$pieData = [];
+
+while ($row = $pieQuery->fetch_assoc()) {
+    $pieData[] = $row;
+}
+
+//bar
+$barQuery = $conn->query("SELECT name, price FROM products WHERE seller_id='$seller_id'");
+$barRes = $conn->query($barQuery);
+$barData = [];
